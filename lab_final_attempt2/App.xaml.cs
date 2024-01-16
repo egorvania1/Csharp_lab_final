2c3a0bb450799e68579ebf060df4e7fee3edba4e
SwankyCentaur33 <egorvania1@gmail.com>
2024-01-16 15:28:06 +0000

    Добавьте файлы проекта.

---
+++ lab_final_attempt2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace lab_final_attempt2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
